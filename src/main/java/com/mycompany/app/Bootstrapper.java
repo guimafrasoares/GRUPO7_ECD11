2da48a95e031553374fbc9cea133e283b6d1324a
adriano-gomes-dev <124310443+adriano-gomes-dev@users.noreply.github.com>
2024-10-19 10:44:01 -0300

    alteracao estrutura de diretorios e reexecutado comandos mvn

---
+++ src/main/java/com/mycompany/app/Bootstrapper.java
@@ -0,0 +1,9 @@
+package com.mycompany.app;
+
+public class Bootstrapper {
+
+	public static void main(String[] args) {
+		HelloWorld hw = new HelloWorld("John");
+		System.out.println(hw.getHello());
+	}
+}
---
+++ src/test/java/com/mycompany/app/HelloTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloTest {
+
+	@Test
+	void testGetHello() {
+		HelloWorld hw = new HelloWorld("Guilherme");	
+		assertEquals("Hello world, Guilherme", hw.getHello());
+	}
+}
